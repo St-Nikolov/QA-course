60b3c47e481a9cf06a7327e8d38e8d5a8bc0f1e6
St-nikolov <satanislaw@yahoo.com>
2020-12-06 19:23:35 +0200

    Lecture14 homework - exceptions

--- QA java course/src/main/java/com/homeworks/company/ET.java
+++ QA java course/src/main/java/com/homeworks/company/ET.java
@@ -1,8 +1,11 @@
 package com.homeworks.company;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.codehaus.plexus.util.StringUtils;
 
 @Getter
+@Slf4j
 public class ET extends Company implements TaxPayer{
 
     private String nameOfTheOwner;
@@ -24,9 +27,34 @@ public class ET extends Company implements TaxPayer{
         return getActualResult()*0.10;
     }
 
-        public double getActualResult(){
+    public double getActualResult(){
         return getActualCapital() - getInitialCapital();
     }
+
+    public int getDailyRevenue(String calculationPeriodInDays, int revenueForPeriod) throws RevenueCalculationException{
+        try {
+            return revenueForPeriod/Integer.parseInt(calculationPeriodInDays);
+        } catch (NumberFormatException e) {
+            throw new RevenueCalculationException("Period parameter should contain only digits");
+        } catch (ArithmeticException e) {
+            throw new RevenueCalculationException("Period parameter cannot be zero",e);
+        }
+
+    }
+
+    public int getDailyRevenueChecked(String calculationPeriodInDays, int revenueForPeriod) {
+        if (!isNumber(calculationPeriodInDays)) {
+            log.error("Period parameter should contain only digits");
+            return -1;
+        }
+        int days = Integer.parseInt(calculationPeriodInDays);
+        if (days==0) {
+            log.error("Period parameter cannot be zero");
+            return -2;
+        }
+        return revenueForPeriod/days;
+    }
+
 
     protected void setNameOfTheOwner(String nameOfTheOwner) {
         this.nameOfTheOwner = nameOfTheOwner;
@@ -38,5 +66,12 @@ public class ET extends Company implements TaxPayer{
 
     protected void setActualCapital(double actualCapital) {
         this.actualCapital = actualCapital;
+    }
+
+    private boolean isNumber(String number){
+//        return number.matches("[0-9] + ([0-9]+)?");
+//       return StringUtils.isNumeric(number);
+
+       return number.matches("\\d+(\\d+)?");
     }
 }
--- QA java course/src/main/java/com/homeworks/company/Main.java
+++ QA java course/src/main/java/com/homeworks/company/Main.java
@@ -8,5 +8,18 @@ public class Main {
         ET et1 = new ET("Balyk","20.04.2022","1234567890","Kancho",1234.50);
         et1.setActualCapital(51234.50);
         log.info("Actual result for \"{}\" is {}, and the tax it has to pay is {}",et1.getName(),et1.getActualResult(),et1.getTax());
+
+        int dailyRevenue;
+        try {
+            dailyRevenue = et1.getDailyRevenue("360", 150700);
+            log.info("Daily revenue of {} is {}", et1.getName(), dailyRevenue);
+        } catch (RevenueCalculationException e) {
+            log.error(e.getMessage());
+        }
+
+        dailyRevenue = et1.getDailyRevenueChecked("555",150700);
+        if (dailyRevenue>=0) log.info("Daily revenue of {} is {}", et1.getName(), dailyRevenue);
+
+
     }
 }
